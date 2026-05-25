74f35210c6eb4709ec992639ae290e49a3014e31
Ricardo Ortega <rikrdo.ortega@gmail.com>
2026-05-24 22:57:27 -0600

    feat: add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Hero from './components/Hero'
 import Nav from './components/Nav'
 
 export default function App() {
   return (
     <div>
       <Nav />
+      <Hero />
     </div>
   )
 }
